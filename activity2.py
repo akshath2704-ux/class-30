221bfe66997de123135b32002bf05b4cfa3bfa5c
akshath2704-ux <akshath2704@gmail.com>
2026-02-25 19:00:34 +0800

    initial commit

---
+++ activity2.py
@@ -0,0 +1,29 @@
+from abc import ABC ,abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def move(self):
+        pass
+class human(Animal):
+    def move (self):
+        print("i can walk and run ")
+class snake(Animal):
+    def move(self):
+        print("i can crawl")
+class dog(Animal):
+    def move(self):
+        print("i can bark")
+class lion(Animal):
+    def move(self):
+        print("i can roar")
+
+r=human()
+r.move()
+
+k=snake()
+k.move()
+
+r=dog()
+r.move()
+
+k=lion()
+k.move()
